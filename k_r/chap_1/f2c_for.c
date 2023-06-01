3bbd46822af5e0ca4da0602b8ceb5782d78127f9
Stephen Smoogen <ssmoogen@redhat.com>
2023-06-01 12:09:43 -0400

    Add in reverse loop for Exercise 1.5

--- k_r/chap_1/f2c_for.c
+++ k_r/chap_1/f2c_for.c
@@ -1,13 +1,20 @@
 /*
   K&R 2nd edition, pg 13 for example.
   Program to use a for loop to do similar work as f2c.
+
+  Add in a reverse loop to go from 300 to 0.
 */
 
 #include <stdio.h>
 int main(){
     int fahr;
+
     printf("Fahrenheit to Celsius Conversion\n");
     for (fahr = 0; fahr <= 300; fahr += 20 )
+	printf("%3d\t=>\t%6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+   
+    printf("\nReverse Fahrenheit to Celsius Conversion\n");
+    for (fahr = 300; fahr >= 0; fahr -= 20 )
 	printf("%3d\t=>\t%6.1f\n",fahr,(5.0/9.0)*(fahr-32));
 
 }
