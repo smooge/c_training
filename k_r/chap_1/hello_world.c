d6470fb1618425f9f6eef3a7f5015dfa38b06837
Stephen Smoogen <ssmoogen@redhat.com>
2023-06-01 09:29:07 -0400

    Fix hello_world to work with newer than 1977 cc Add simple Makefile

---
+++ k_r/chap_1/hello_world.c
@@ -0,0 +1,12 @@
+/*
+  The first program of K&R 2nd edition is the classic "hello.c". This
+  version is updated to work with newer ISO C standards.
+ */
+
+#include <stdio.h>  // needed for printf
+#include <stdlib.h> // needed for exit
+
+int main(){
+    printf("hello, world.\n");
+    exit(0);
+}
