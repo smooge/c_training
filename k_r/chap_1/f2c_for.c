34c2d10c3938c74bbdf6398e7dc5f37aa35c609e
Stephen Smoogen <ssmoogen@redhat.com>
2023-06-01 12:06:14 -0400

    Remove warning about no int on main

--- k_r/chap_1/f2c_for.c
+++ k_r/chap_1/f2c_for.c
@@ -4,10 +4,10 @@
 */
 
 #include <stdio.h>
-main(){
+int main(){
     int fahr;
     printf("Fahrenheit to Celsius Conversion\n");
-    for (fahr = 0; fahr <= 300; fahr +=20 )
+    for (fahr = 0; fahr <= 300; fahr += 20 )
 	printf("%3d\t=>\t%6.1f\n",fahr,(5.0/9.0)*(fahr-32));
 
 }
