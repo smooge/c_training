8e6bcdc61973e87731ca099d121a0613f5627734
Stephen Smoogen <ssmoogen@redhat.com>
2023-06-01 11:28:38 -0400

    Put in initial K&R fahrenheit to celsius. Add in f2c to Makefile.

---
+++ k_r/chap_1/f2c.c
@@ -0,0 +1,23 @@
+/*
+  Sample Fahrenheit to Celsius from K&R C 2nd edition Book.
+  Page 9
+ */
+
+#include <stdio.h>
+
+main(){
+    int fahr, celsius;
+    int lower,upper, step;
+
+    lower = 0;   // Lower limit of temperature table
+    upper = 550; // Upper limit
+    step = 20;
+
+    fahr = lower;
+    printf("Fahrenheit to Celsius Conversion\n");
+    while (fahr <= upper) {
+	celsius = 5 * ( fahr-32) / 9;
+	printf("%d\t=>\t%d\n",fahr,celsius);
+	fahr = fahr + step;
+    }
+}
