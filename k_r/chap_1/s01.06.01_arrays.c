d72aef3981270a1b69998b4d134a2976269a3345
Stephen Smoogen <ssmoogen@redhat.com>
2023-07-13 16:40:00 -0400

    Input sample from K&R C, pg22 S01.06
    This program gives an example array and then uses some counting
    methods to count the number of each digit in a file. The output needs
    work as it does not cover say what digit is being counted where. I also deviated in this initial checkin from the original text to cover some warnings from splint on char/int math.
    
    Signed-off-by: Stephen Smoogen <ssmoogen@redhat.com>

---
+++ k_r/chap_1/s01.06.01_arrays.c
@@ -0,0 +1,37 @@
+/*
+  Example program from K&R C Programming, 2nd edition, pg 22.
+
+  Program is to count blank (space, tab, newline), all other
+  characters, and the number of times each decimal digit shows up.
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int c, i, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+
+    for (i=0; i<10; ++i){
+	ndigit[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF){
+	if ( c >= (int) '0' && c <= (int) '9')
+	    ++ndigit[ c - (int) '0']; // Legal but aaaah
+	else if ((char) c == ' ' || (char) c == '\n' || (char) c == '\t')
+	    ++nwhite;
+	else
+	    ++nother;
+    }
+
+    printf("digits = ");
+    for (i = 0; i < 10; i++)
+	printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n",nwhite,nother);
+    
+    exit(0);
+}
