5d39bb3650068390493fc7237cf6444897320e34
Stephen Smoogen <ssmoogen@redhat.com>
2023-07-13 15:50:13 -0400

    Import example s01.05.04 from K&R pg 20
    This is another version of the 'counting' character program in
    K&R. This version is closer to the wc program in counting some items
    and outputting basic characters, words, and lines in a file. However
    it does not have any logic to open the file or limit what output is to
    be given.
    
    Signed-off-by: Stephen Smoogen <ssmoogen@redhat.com>

---
+++ k_r/chap_1/s01.05.04_count.c
@@ -0,0 +1,38 @@
+/*
+  K&R 2nd edition, pg 20
+
+  This program will do a simple count of 'words' in a file where a
+  word is defined as any sequence of characters which is not a blank
+  or newline.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* preprocessor values */
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+/* count lines, words, and characters in input */
+
+int main(){
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0; /* set initial counts to 0 */
+
+    while ((c = getchar()) != EOF){
+	++nc;
+	if (c == '\n')
+	    ++nl;
+	if (c == ' ' || c == '\n' || c == '\t')
+	    state = OUT;
+	else if (state == OUT ) {
+	    state = IN;
+	    ++nw;
+	}
+    }
+
+    printf("%d %d %d\n", nl, nw, nc);
+    exit(0);
+}
