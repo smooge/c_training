94d7c2d3bba5e2facecaa9b012463e2d48164e09
Stephen Smoogen <ssmoogen@redhat.com>
2023-09-19 12:47:04 -0400

    Finish the example code from K&R C pg 32.
    This program is a riff on the previous 'longest' string program but is
    meant to show how extern can work inside a function for 'global'
    variables. This code does not pass splint testing at all as it sees
    all the variables as shadows.
    
    Signed-off-by: Stephen Smoogen <ssmoogen@redhat.com>

---
+++ k_r/chap_1/s01.10.01_longest.c
@@ -0,0 +1,73 @@
+/* 
+ * Program from K&R 2nd edition pg 32 Meant to show how extern
+ * variables work.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 1000 /* maximum input line size */
+
+int max = 0; /* maximum length seen so far */
+char line[MAXLINE];  /* current input line */
+char longest[MAXLINE]; /* longest line is stored here */
+
+
+static int getline(void);
+static void copy(void);
+
+/* print the longest line */
+int main(){
+    int len = 0; /* current line length */
+    extern int max; /* maximum length seen so far */
+    extern char line[];
+    extern char longest[];
+    int i=0;
+
+    // zero out our two strings
+    for (i=0;i<MAXLINE;i++){
+	line[i]='\0';
+	longest[i]='\0';
+    }
+    
+    do {
+	len = getline();
+	if (len > max) {
+	    max = len;
+	    copy();
+	}
+    } while ( len > 0);
+    if (max > 0){ /* there was a line longer than 0 */
+	printf("Longest line had %d chars\n",max);
+	printf("%s\n",longest);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+/* getline: read a line into string array, return length */
+int getline(void){
+    int c = 0;
+    int i = 0;
+    extern char line[];
+
+    for (i=0; i<MAXLINE-1 && (c=getchar())!=EOF && (char) c!='\n'; ++i)
+	line[i]=(char) c;
+    if ((char) c == '\n'){
+	line[i] = (char) c;
+	i++;
+    }
+    line[i]= '\0';
+    return i;
+}
+
+/* copy: copy a string 'from' into 'to'; assume to is big enough. */
+void copy(void){
+    int i=0;
+    extern char line[], longest[];
+
+    do {
+	longest[i] = line[i];
+	i++;
+    } while (line[i] != '\0');
+
+}
